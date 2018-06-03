f60625483bc2e9a75889260d90f7da081a8e8860
Daniel Duong <daniel.duong@outlook.com>
2018-06-03 18:55:07 +0700

    Creator can be set to NULL.
    It means the user doesn't exist anymore

--- hnews/posts/models.py
+++ hnews/posts/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    creator = models.ForeignKey(User, related_name='posts')
+    creator = models.ForeignKey(
+        User,
+        related_name='posts',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     creation_date = models.DateTimeField(auto_now_add=True)
     url = models.URLField()
