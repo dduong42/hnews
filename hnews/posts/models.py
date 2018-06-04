413deb5d3a7e0bf3ae2adea5356dee398a4870f4
Daniel Duong <daniel.duong@outlook.com>
2018-06-04 11:23:18 +0700

    Models done, add migrations.

--- hnews/posts/models.py
+++ hnews/posts/models.py
@@ -11,3 +11,29 @@ class Post(models.Model):
     )
     creation_date = models.DateTimeField(auto_now_add=True)
     url = models.URLField()
+    upvotes = models.ManyToManyField(User, through='PostUpvote')
+    title = models.CharField(max_length=256)
+
+
+class PostUpvote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='post_upvotes', on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(
+        User,
+        related_name='comments',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    parent = models.ForeignKey('Comment', related_name='replies', on_delete=models.CASCADE, null=True, default=None)
+    content = models.TextField(null=True)
+    upvotes = models.ManyToManyField(User, through='CommentUpvote')
+
+
+class CommentUpvote(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='comment_upvotes', on_delete=models.CASCADE)
