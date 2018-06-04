d289b3e2c1e96182f691153a856e39ed9b7b7dd0
Daniel Duong <daniel.duong@outlook.com>
2018-06-04 13:39:21 +0700

    Add management command to create dev data

---
+++ hnews/posts/management/commands/create_dev_data.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from hnews.posts.models import Post
+
+
+class Command(BaseCommand):
+    help = 'Create data for dev'
+
+    def handle(self, *args, **options):
+        jean, _ = User.objects.get_or_create(
+            username='jean',
+            email='jean@example.com',
+        )
+        jean.set_password('password1337')
+        jean.save()
+        jack, _ = User.objects.get_or_create(
+            username='jack',
+            email='jack@example.com',
+        )
+        jack.set_password('password1337')
+        jack.save()
+        james, _ = User.objects.get_or_create(
+            username='james',
+            email='james@example.com',
+        )
+        james.set_password('password1337')
+        james.save()
+
+        microsoft, _ = Post.objects.get_or_create(
+            title='Microsoft Is Said to Have Agreed to Acquire GitHub',
+            url='https://www.bloomberg.com/news/articles/2018-06-03/microsoft-is-said-to-have-agreed-to-acquire-coding-site-github?',
+            creator=jean,
+        )
+        gitlab, _ = Post.objects.get_or_create(
+            title='GitLab sees huge spike in project imports',
+            url='https://monitor.gitlab.net/dashboard/db/github-importer?orgId=1',
+            creator=jack,
+        )
+        facebook, _ = Post.objects.get_or_create(
+            title='Facebook Gave Device Makers Deep Access to Data on Users and Friends',
+            url='https://www.nytimes.com/interactive/2018/06/03/technology/facebook-device-partners-users-friends-data.html',
+            creator=james,
+        )
